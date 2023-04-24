6e3bf79d31921879c4d0d639b9f704ddbe3cf2f4
Misael <li.misael.sanchez@gmail.com>
2023-04-24 12:06:11 -0600

    ionic crash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FirebaseX } from "@ionic-native/firebase-x/ngx";
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, FirebaseX, GooglePlus],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { FirebaseX } from "@ionic-native/firebase-x/ngx";
+import { Platform } from '@ionic/angular';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  token: string | null;
+  simulateError: any;
 
-  constructor() {}
+  constructor(private firebase: FirebaseX, private googlePlus: GooglePlus) {
+    this.token = '';
+  }
 
+  getToken(){
+    this.firebase.getToken().then(token => {
+      this.token = token;
+    });
+  }
+
+  setPerformanceEnable(){
+    this.firebase.setAnalyticsCollectionEnabled(true);
+    this.firebase.setPerformanceCollectionEnabled(true);
+  }
+
+  setUserId(){
+    this.firebase.setCrashlyticsUserId(String(this.token));
+  }
+
+  sendLogErrorToCrash(){
+    this.firebase.logMessage("Crash test!");
+  }
+
+  crashApp(){
+    this.firebase.sendCrash();
+  }
+
+  normalexception(success:any, error:any){
+    //this.loginGoogleAndroid();
+  }
+
+  async loginGoogleAndroid() {
+    /*onst res = await this.googlePlus.login({
+      'webClientId': '523893424469-nmnerih50vb2u7a16pajmtuhn6h3crkg.apps.googleusercontent.com',
+      'offline': true
+    });*/
+
+    this.firebase.authenticateUserWithGoogle('523893424469-nmnerih50vb2u7a16pajmtuhn6h3crkg.apps.googleusercontent.com')
+    .then(res => {
+      console.error('Response!', res)
+    }).catch(error => {
+      console.error('error', error)
+    })
+}
 }
